8a6444e48585cb7decd7c96fcf6463911f5f8aa9
LouisPauchet <louis.pauchet@insa-rouen.fr>
2025-12-31 00:18:52 +0100

    More Linting

--- app/utils/error.py
+++ app/utils/error.py
@@ -13,5 +13,7 @@ def handle_validation_error(model: Type[BaseModel], **kwargs: Any) -> None:
         logger.error("Invalid request format: {}, - Error UUID : {}".format(e, uid))
         raise HTTPException(
             status_code=400,
-            detail="Invalid request format. Contact the support giving them the following error code {}".format(uid),
+            detail="Invalid request format. Contact the support giving them the following error code {}".format(
+                uid
+            ),
         )
