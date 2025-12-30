984add6a0bbfed18702ebebacdeb71145679bc8c
LouisPauchet <louis.pauchet@insa-rouen.fr>
2025-12-30 16:05:20 +0100

    Add Routeur Station Info

--- app/api/v3/router.py
+++ app/api/v3/router.py
@@ -1,5 +1,11 @@
 from fastapi import APIRouter
 from app.api.v3.endpoints import forecast
+from app.api.v3.endpoints import stations_info
 
 api_router = APIRouter()
-api_router.include_router(forecast.router, prefix="/forecast", tags=["forecast"])
+api_router.include_router(
+    forecast.router, prefix="/forecast", tags=["Weather Forecast"]
+)
+api_router.include_router(
+    stations_info.router, prefix="/station-status", tags=["Station Status"]
+)
