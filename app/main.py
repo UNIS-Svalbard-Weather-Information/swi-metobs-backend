ace22c79f9dfc119bf1dae0d43267a7873cc6330
LouisPauchet <louis.pauchet@insa-rouen.fr>
2026-01-25 20:08:27 +0100

    feat: CORS Policy setup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
-
 from app.api.v3.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI(title="SWI MetObs API", version="v3.0.0")
+
+# Read the environment variable for allowed origins
+allowed_origins = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router, prefix="/v3")
